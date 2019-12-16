871cfc32630122e66094bea7b99e3484443600ae
adelmattih <benmattih.adel@gmail.com>
2019-12-16 23:13:32 +0100

    Upload New File

---
+++ essaiQuestion.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 19:44:19 2019
+
+@author: paul
+"""
+"""
+Descriptif du programme:
+    
+Le programme pose la question a l'utilisateur du sexe du personnage, et réponds 
+en fonction.
+
+"""
+
+print("votre personnage est-il une femme?")
+rep=input()
+if rep=="oui":
+    print("votre personnage est une femme.")
+else:
+    print("votre personnage est un homme.")
